dac59f6f3177da73563051126784ab5b99616e43
Chaiminda Chandrasekara <chaminda.chandrasekara@ifs.com>
2023-11-04 19:50:59 +0100

    Add q storage

---
+++ iac/storage.tf
@@ -0,0 +1,17 @@
+resource "azurerm_storage_account" "queue" {
+  name                     = "${var.PREFIX}${var.PROJECT}${replace(var.ENVNAME, "-", "")}queuest"
+  resource_group_name      = azurerm_resource_group.instancerg.name
+  location                 = azurerm_resource_group.instancerg.location
+  account_tier                     = "Standard"
+  account_replication_type         = "LRS"
+  account_kind                     = "StorageV2"
+  access_tier                      = "Hot"
+  allow_nested_items_to_be_public  = false
+  min_tls_version                  = "TLS1_2"
+  cross_tenant_replication_enabled = false
+}
+
+resource "azurerm_storage_queue" "video" {
+  name                 = "demovideoqueue"
+  storage_account_name = azurerm_storage_account.queue.name
+}
