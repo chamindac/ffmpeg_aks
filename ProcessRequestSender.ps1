a045baa9afc371f8e874873acc93faef7fdfbc2b
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2023-11-08 17:17:31 +0100

    Queue multiple process requests

---
+++ ProcessRequestSender.ps1
@@ -0,0 +1,23 @@
+﻿$assetsFilename = 'assetsToProcess.json'
+
+$queueStorageAccKey = az storage account keys list -g ch-video-dev-euw-001-rg -n chvideodeveuw001queuest --query [0].value -o tsv
+
+$expirySaSKey = (Get-Date).AddMinutes(30).ToString('yyyy-MM-ddTHH:mmZ') 
+$ErrorActionPreference = 'SilentlyContinue'
+$queueSaSKey= az storage queue generate-sas --account-key $queueStorageAccKey --account-name chvideodeveuw001queuest -n demovideoqueue --permissions apru --expiry $expirySaSKey --https-only -o tsv
+$ErrorActionPreference = 'Continue'
+
+$assets = Get-Content -Path "$PSScriptRoot/$assetsFilename" | ConvertFrom-Json
+
+foreach($asset in $assets)
+{
+    $assetMessage = '<QueueMessage><MessageText>' + ($asset | ConvertTo-Json) + '</MessageText></QueueMessage>'
+
+    $head = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $head = @{}
+    $head.Add("-ms-version","2020-04-08")
+
+    $url = 'https://chvideodeveuw001queuest.queue.core.windows.net/demovideoqueue/messages?' + $queueSaSKey
+
+    Invoke-WebRequest -Method Post -Uri $url -ContentType 'text/plain' -Body $assetMessage -Headers $head
+}
