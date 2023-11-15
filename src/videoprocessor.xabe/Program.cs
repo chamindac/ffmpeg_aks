4fe24ecebf0fe97c24d7d27b75dc0ceb01f90463
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2023-11-15 18:20:47 +0100

    download blob for .NET video processor

---
+++ src/videoprocessor.xabe/Program.cs
@@ -0,0 +1,93 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage;
+using Azure.Storage.Blobs.Models;
+
+namespace videoprocessor.xabe;
+
+class Program
+{
+    const string SourceStorageName = "cheuw001assetsstcool";
+    const string SourceStorageKey = "shouldbesecretinAKS";
+    const string OriginalAssetBlobName = "original";
+
+    static void Main(string[] args)
+    {
+        string? mediaPath = Environment.GetEnvironmentVariable("MEDIA_PATH");
+
+        if (mediaPath is not null)
+        {
+            string assetId = "bb5ab2dd-f89c-4689-976b-0de2fce614ec";
+            string assetContianer = "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3";
+            string assetFolderPath = Path.Combine(mediaPath, assetId);
+
+            string assetPath = DownloadOrginalAsset(assetContianer, assetFolderPath, assetId);
+
+            string[] assetFiles = Directory.GetFiles(assetFolderPath);
+
+
+            Console.WriteLine($"Media path is: {mediaPath}");
+            foreach (var assetFile in assetFiles)
+            {
+                Console.WriteLine($": {assetFile}");
+            }
+            
+        }
+    }
+
+    private static BlobServiceClient GetBlobServiceClient(string accountName, string accountKey)
+    {
+        Azure.Storage.StorageSharedKeyCredential sharedKeyCredential =
+            new StorageSharedKeyCredential(accountName, accountKey);
+
+        string blobUri = "https://" + accountName + ".blob.core.windows.net";
+
+        return new
+            (new Uri(blobUri), sharedKeyCredential);
+
+
+    }
+
+    public static string DownloadOrginalAsset(string assetCotainer,
+        string assetFolderPath,
+        string assetId)
+    {
+
+        BlobServiceClient blobServiceClient = GetBlobServiceClient(SourceStorageName, SourceStorageKey);
+        BlobClient blobClient = blobServiceClient
+                .GetBlobContainerClient(assetCotainer)
+                .GetBlobClient($"{assetId}/{OriginalAssetBlobName}");
+
+        if (Directory.Exists(assetFolderPath))
+        {
+            Directory.Delete(assetFolderPath, true);
+        }
+
+        Directory.CreateDirectory(assetFolderPath);
+
+        string localFilePath = Path.Combine(assetFolderPath, assetId);
+        FileStream fileStream = File.OpenWrite(localFilePath);
+
+        var transferOptions = new StorageTransferOptions
+        {
+            // Set the maximum number of parallel transfer workers
+            MaximumConcurrency = 5,
+
+            // Set the initial transfer length to 8 MiB
+            InitialTransferSize = 8 * 1024 * 1024,
+
+            // Set the maximum length of a transfer to 4 MiB
+            MaximumTransferSize = 4 * 1024 * 1024
+        };
+
+        BlobDownloadToOptions downloadOptions = new BlobDownloadToOptions()
+        {
+            TransferOptions = transferOptions
+        };
+
+        blobClient.DownloadTo(fileStream, downloadOptions);
+
+        fileStream.Close();
+
+        return localFilePath;
+    }
+}
---
+++ src/videoprocessor.xabe/setup.sh
@@ -0,0 +1,102 @@
+echo "Updating all packages..."
+apt-get update && apt-get upgrade -y
+echo "Updating all packages completed."
+
+# echo "Setting up wget..."
+# apt-get install wget
+# echo "Setting up wget completed."
+
+# echo "Setting up yq..."
+# wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64
+# chmod a+x /usr/local/bin/yq
+# yq --version
+# echo "Setting up yq completed."
+
+echo "setting up .NET..."
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        ca-certificates \
+        libc6 \
+        libgcc1 \
+        libgcc-s1 \
+        libgssapi-krb5-2 \
+        libicu70 \
+        liblttng-ust1 \
+        libssl3 \
+        libstdc++6 \
+        libunwind8 \
+        zlib1g \
+    && rm -rf /var/lib/apt/lists/*
+curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin -Channel 8.0 -Runtime dotnet -InstallDir /usr/share/dotnet
+ln -s /usr/share/dotnet/dotnet /usr/bin/dotnet
+# RUN apt-get install -y dotnet-runtime-8.0 # not available yet
+echo "setting up .NET completed."
+
+# echo "Setting up Azure CLI..."
+# curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+# echo "Setting up Azure CLI completed."
+
+# echo "az login..."
+# az login --service-principal -u spnappid -p spnapppwd --tenant tenantid
+# az account set --subscription "subscriptionid"
+# az group list -o table
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for future use -copy below somewhere
+
+# update all
+#RUN apt-get update && apt-get upgrade -y
+
+# echo "Setting up uuid-runtime..."
+# apt-get install uuid-runtime
+# echo "Setting up uuid-runtime completed."
+
+
+# install required packages
+# RUN apt-get install -y -qq --no-install-recommends \
+#     apt-transport-https \
+#     apt-utils \
+#     ca-certificates \
+#     curl \
+#     unzip \
+#     wget \
+#     git \
+#     iputils-ping \
+#     jq \
+#     lsb-release \
+#     software-properties-common \ 
+#     sudo \
+#     ghostscript \
+#     freetype2-doc
+
+# # install git lfs extension
+# RUN curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+# RUN apt-get install git-lfs -y -qq --no-install-recommends
+
+# # install PowerShell
+# RUN wget -q https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/powershell_7.3.6-1.deb_amd64.deb
+# RUN dpkg -i powershell_7.3.6-1.deb_amd64.deb
+# RUN apt-get install -f
+
+# # Install Terraform
+# RUN wget -q https://releases.hashicorp.com/terraform/1.6.2/terraform_1.6.2_linux_amd64.zip \
+#     && unzip terraform_1.6.2_linux_amd64.zip \
+#     && mv terraform /usr/local/bin/ \
+#     && rm terraform_1.6.2_linux_amd64.zip
+
+# # install AzDoURE CLI
+# RUN curl -sL https://aka.ms/InstallAzureCLIDeb | bash
