212791121a56a8d0a30e88bc2404a772751c3a18
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2023-11-21 10:09:12 +0100

    variable for queue name

--- ProcessRequestSender.ps1
+++ ProcessRequestSender.ps1
@@ -1,10 +1,11 @@
 ﻿$assetsFilename = 'assetsToProcess.json'
+$queueName = 'demovideoqueue'
 
 $queueStorageAccKey = az storage account keys list -g ch-video-dev-euw-001-rg -n chvideodeveuw001queuest --query [0].value -o tsv
 
 $expirySaSKey = (Get-Date).AddMinutes(30).ToString('yyyy-MM-ddTHH:mmZ') 
 $ErrorActionPreference = 'SilentlyContinue'
-$queueSaSKey= az storage queue generate-sas --account-key $queueStorageAccKey --account-name chvideodeveuw001queuest -n demovideoqueue --permissions apru --expiry $expirySaSKey --https-only -o tsv
+$queueSaSKey= az storage queue generate-sas --account-key $queueStorageAccKey --account-name chvideodeveuw001queuest -n $queueName --permissions apru --expiry $expirySaSKey --https-only -o tsv
 $ErrorActionPreference = 'Continue'
 
 $assets = Get-Content -Path "$PSScriptRoot/$assetsFilename" | ConvertFrom-Json
@@ -19,7 +20,7 @@ foreach($asset in $assets)
     $head = @{}
     $head.Add("-ms-version","2020-04-08")
 
-    $url = 'https://chvideodeveuw001queuest.queue.core.windows.net/demovideoqueue/messages?' + $queueSaSKey
+    $url = 'https://chvideodeveuw001queuest.queue.core.windows.net/' + $queueName + '/messages?' + $queueSaSKey
 
     Invoke-WebRequest -Method Post -Uri $url -ContentType 'text/plain' -Body $assetMessage -Headers $head
 
