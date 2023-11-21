d9d23c00ccb7073cd6e821b0b162eb35c4451f40
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2023-11-21 10:57:56 +0100

    video processor explained in comments

--- src/videoprocessor.xabe/Program.cs
+++ src/videoprocessor.xabe/Program.cs
@@ -28,14 +28,13 @@ class Program
 
     static async Task Main(string[] args)
     {
+        // Use mounted volume path for container
         string? mediaPath = Environment.GetEnvironmentVariable("MEDIA_PATH");
-
-        //"C:\\temp\\videos";
 
         if (mediaPath is not null)
         {
+            // Get first message from queue with 4 hour hide time for message and exist if no messages
             QueueClient queueClient = new(QueueStorageConnection, QueueName);
-
             QueueMessage message = await queueClient.ReceiveMessageAsync(new TimeSpan(0, 4, 0)); // Max four hours to process
 
             if (message is null)
@@ -46,6 +45,7 @@ class Program
 
             Console.WriteLine($"Received message{message.Body}");
 
+            // Extract received message body
             JsonSerializerOptions serializeOptions = new JsonSerializerOptions()
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
@@ -60,20 +60,14 @@ class Program
                     OutFilePrefix = string.Empty
                 };
 
-            //AssetMessage assetMessage = new()
-            //{
-            //    AssetContianerName = "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
-            //    AssetId = "a3a45e98-a2ce-4b37-84ab-3fbd1cae624f",
-            //    OriginalAssetBlobName = "original",
-            //    OutFilePrefix = "friendseating"
-            //};
-
-            Console.WriteLine($"Asset Id: {assetMessage.AssetId}");
-            Console.WriteLine($"Asset Container Name: {assetMessage.AssetContianerName}");
+            // Create video download folder
+            Console.WriteLine($"Video Asset Id: {assetMessage.AssetId}");
+            Console.WriteLine($"Video Asset Container Name: {assetMessage.AssetContianerName}");
             string assetFolderPath = Path.Combine(mediaPath, assetMessage.AssetId);
 
             try
             {
+                // Download original video file
                 Stopwatch downloadTimer = Stopwatch.StartNew();
                 string assetPath = await DownloadOrginalAsset(assetMessage.AssetContianerName,
                     assetFolderPath,
@@ -82,45 +76,57 @@ class Program
 
                 downloadTimer.Stop();
                 Console.WriteLine($"Media path is: {mediaPath}");
-                Console.WriteLine($"Asset path is: {assetPath}");
+                Console.WriteLine($"Video asset path is: {assetPath}");
 
+                // Obtain video file size in MBs and duration
                 FileInfo assetInfo = new FileInfo(assetPath);
                 long assetSize = assetInfo.Length / (1024 * 1024);
                 string outputFolderPath = Path.Combine(assetFolderPath, OutputFolderName);
-
-                Directory.CreateDirectory(outputFolderPath);
+                Console.WriteLine($"Video file size is: {assetSize} MBs");
 
                 IMediaInfo info = await FFmpeg.GetMediaInfo(assetPath);
-                Console.WriteLine($"Asset duration is: {info.Duration}");
+                Console.WriteLine($"Video duration is: {info.Duration}");
 
+                // Create output folder to keep generated files for uploading to target
+                Directory.CreateDirectory(outputFolderPath);
+
+                // Obtain vidoe stream and set 720p preview size, based on horizontal or vertical video
                 IVideoStream? videoStream = info.VideoStreams.FirstOrDefault();
                 if (videoStream?.Height > videoStream?.Width)
                 {
+                    // Vertical
                     videoStream
                         .SetSize(720, 1280)
                         .SetCodec(VideoCodec.libx264);
                 }
                 else
                 {
+                    // Hortizontal
                     videoStream?
                         .SetSize(VideoSize.Hd720)
                         .SetCodec(VideoCodec.libx264);
                 }
 
+                // Obtain audio stream
                 IStream? audioStream = info.AudioStreams.FirstOrDefault();
 
+                // Generate 720p video
                 IConversionResult resultVideo = await FFmpeg.Conversions.New()
                     .AddStream(videoStream, audioStream)
                     .SetOutput(Path.Combine(outputFolderPath, string.Concat(assetMessage.OutFilePrefix, "_720.mp4")))
                     .Start();
 
+                // Delegate for image name
                 Func<string, string> outputFileNameBuilder = (number) => { return Path.Combine(outputFolderPath, string.Concat(assetMessage.OutFilePrefix, number, ".png")); };
 
+                // Obtain video file for extacting images as png
                 IVideoStream videoStreamForImages = info.VideoStreams.First().SetCodec(VideoCodec.png);
-                int frameExctractionRate = (int)(videoStreamForImages.Framerate * videoStreamForImages.Duration.Seconds) / 10;
+                // Calculate image extraction frame rate, to extract 10 images from the video
+                int frameExctractionRate = (int)(videoStreamForImages.Framerate * videoStreamForImages.Duration.Seconds) / 9; // Divide by 10 gives 11 images
 
                 Console.WriteLine($"Frame extraction rate is:{frameExctractionRate}");
 
+                // Extract images
                 IConversionResult resultImages = await FFmpeg.Conversions.New()
                     .AddStream(videoStreamForImages)
                     .ExtractEveryNthFrame(frameExctractionRate, outputFileNameBuilder)
@@ -130,17 +136,19 @@ class Program
                 Console.WriteLine($"Image covertion duration is: {resultImages.Duration}");
                 Console.WriteLine($"Process duration is: {resultImages.Duration.Add(resultVideo.Duration)}");
 
+                // Upload generated 720p video and exxtracted images to target blob storage
                 Stopwatch uploadTimer = Stopwatch.StartNew();
                 BlobServiceClient destinationBlobServiceClient = GetBlobServiceClient(destinationStorageName,
                     destinationStorageKey);
 
                 BlobContainerClient destinationContainer = await CreateDestinationContainer(destinationBlobServiceClient,
-                    string.Concat("video-", assetMessage.AssetId));
+                    string.Concat("dotnet-", assetMessage.AssetId));
 
                 await UploadGeneratedAssets(destinationContainer, outputFolderPath);
                 uploadTimer.Stop();
                 Console.WriteLine($"File upload duration is:{uploadTimer.Elapsed.TotalSeconds} seconds");
 
+                // Generate process time info file
                 string processTimeInfoFileName = "processtime.txt";
                 string processTimeInfoFilePath = Path.Combine(outputFolderPath, processTimeInfoFileName);
                 StreamWriter processTimeInfoFileStream = File.CreateText(processTimeInfoFilePath);
@@ -150,9 +158,11 @@ class Program
                 await processTimeInfoFileStream.WriteLineAsync($"Upload duration is:{uploadTimer.Elapsed.TotalSeconds} seconds");
                 processTimeInfoFileStream.Close();
 
+                // Upload process time info file
                 BlobClient blob = destinationContainer.GetBlobClient(processTimeInfoFileName);
                 await blob.UploadAsync(processTimeInfoFilePath, overwrite:true);
 
+                // Remove processed message from queue
                 await queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
                 Console.WriteLine($"Successfully processed in {message.DequeueCount} attempt(s). Removing message from the queue...");
             }
@@ -160,6 +170,7 @@ class Program
             {
                 Console.WriteLine(ex.Message);
 
+                // Allow retry message processing for 3 attempts, remove message if 3 attempts and still failing
                 if (message.DequeueCount >= 3)
                 {
                     Console.WriteLine("Failed to process in 3 attempts. Removing message from the queue...");
@@ -175,6 +186,7 @@ class Program
             }
             finally
             {
+                // Clean up downloaded and generated files
                 if (Directory.Exists(assetFolderPath))
                 {
                     Directory.Delete(assetFolderPath, true);
