8b73f0683fdb9832aa0167a23acb69546f8e5e5c
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2024-01-25 15:00:07 +0100

    servie bus data roles

--- iac/servicebus.tf
+++ iac/servicebus.tf
@@ -16,3 +16,17 @@ resource "azurerm_servicebus_queue" "dotnetvideo" {
   requires_duplicate_detection = true
   enable_partitioning          = true
 }
+
+resource "azurerm_role_assignment" "sbq_contributor" {
+  principal_id                     = azurerm_user_assigned_identity.aks.principal_id
+  role_definition_name             = "Azure Service Bus Data Owner"
+  scope                            = azurerm_servicebus_queue.dotnetvideo.id
+  skip_service_principal_aad_check = true
+}
+
+# Assign permission to manage queue data for subscription owners group
+resource "azurerm_role_assignment" "sbq_contributor_subowners" {
+  principal_id                     = data.azuread_group.sub_owners.object_id
+  role_definition_name             = "Azure Service Bus Data Owner"
+  scope                            = azurerm_servicebus_queue.dotnetvideo.id
+}
