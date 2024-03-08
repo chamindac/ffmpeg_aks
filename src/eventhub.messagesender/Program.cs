373cc12f8358cae6bb7d740e44c3a9d9061f1700
Chaminda Chandrasekara <chaminda.chandrasekara@fotoware.com>
2024-03-08 14:46:09 +0100

    processing events implemented

--- src/eventhub.messagesender/Program.cs
+++ src/eventhub.messagesender/Program.cs
@@ -59,26 +59,26 @@ internal class Program
     {
         return new List<string>()
         {
-            """
-                {
-                    "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
-                    "assetId": "bb5ab2dd-f89c-4689-976b-0de2fce614ec",
-                    "originalAssetBlobName": "original",
-                    "sourceStorageAccount": "cheuw001assetsstcool",
-                    "destinationStorageAccount": "cheuw001assetssthot",
-                    "outFilePrefix": "walk",
-                    "commandArgs": [
-                        {
-                        "outFileOptions": "-vf fps=1/4",
-                        "outFileName": "walk_%04d.png"
-                        },
-                        {
-                        "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
-                        "outFileName": "walk_720p.mp4"
-                        }
-                    ]
-                }
-             """,
+            // """
+            //     {
+            //         "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
+            //         "assetId": "bb5ab2dd-f89c-4689-976b-0de2fce614ec",
+            //         "originalAssetBlobName": "original",
+            //         "sourceStorageAccount": "cheuw001assetsstcool",
+            //         "destinationStorageAccount": "cheuw001assetssthot",
+            //         "outFilePrefix": "walk",
+            //         "commandArgs": [
+            //             {
+            //             "outFileOptions": "-vf fps=1/4",
+            //             "outFileName": "walk_%04d.png"
+            //             },
+            //             {
+            //             "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
+            //             "outFileName": "walk_720p.mp4"
+            //             }
+            //         ]
+            //     }
+            //  """,
             """
                 {
                     "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
@@ -98,67 +98,68 @@ internal class Program
                         }
                     ]
                 }
-             """,
-            """
-                {
-                    "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
-                    "assetId": "a3a45e98-a2ce-4b37-84ab-3fbd1cae624f",
-                    "originalAssetBlobName": "original",
-                    "sourceStorageAccount": "cheuw001assetsstcool",
-                    "destinationStorageAccount": "cheuw001assetssthot",
-                    "outFilePrefix": "friendseating",
-                    "commandArgs": [
-                        {
-                        "outFileOptions": "-vf fps=1/4",
-                        "outFileName": "friendseating_%04d.png"
-                        },
-                        {
-                        "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
-                        "outFileName": "friendseating_720p.mp4"
-                        }
-                    ]
-                }
-             """,
-            """
-                {
-                    "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
-                    "assetId": "1e049c87-ce56-4c54-afc8-0c5a01a97bf3",
-                    "originalAssetBlobName": "original",
-                    "sourceStorageAccount": "cheuw001assetsstcool",
-                    "destinationStorageAccount": "cheuw001assetssthot",
-                    "outFilePrefix": "boatnewyork",
-                    "commandArgs": [
-                        {
-                        "outFileOptions": "-vf fps=1/4",
-                        "outFileName": "boatnewyork_%04d.png"
-                        },
-                        {
-                        "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
-                        "outFileName": "boatnewyork_720p.mp4"
-                        }
-                    ]
-                }
-            """,
-            """
-                {
-                    "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
-                    "assetId": "66403f95-2ded-496c-bb50-38713bbe2000",
-                    "originalAssetBlobName": "original",
-                    "sourceStorageAccount": "cheuw001assetsstcool",
-                    "destinationStorageAccount": "cheuw001assetssthot",
-                    "outFilePrefix": "waterstream",
-                    "commandArgs": [
-                        {
-                        "outFileOptions": "-vf fps=1/2",
-                        "outFileName": "waterstream_%04d.png"
-                        },
-                        {
-                        "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 720x1280",
-                        "outFileName": "waterstream_720p.mp4"
-                        }
-                    ]
-                }
-            """
+             """
+            //  ,
+            // """
+            //     {
+            //         "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
+            //         "assetId": "a3a45e98-a2ce-4b37-84ab-3fbd1cae624f",
+            //         "originalAssetBlobName": "original",
+            //         "sourceStorageAccount": "cheuw001assetsstcool",
+            //         "destinationStorageAccount": "cheuw001assetssthot",
+            //         "outFilePrefix": "friendseating",
+            //         "commandArgs": [
+            //             {
+            //             "outFileOptions": "-vf fps=1/4",
+            //             "outFileName": "friendseating_%04d.png"
+            //             },
+            //             {
+            //             "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
+            //             "outFileName": "friendseating_720p.mp4"
+            //             }
+            //         ]
+            //     }
+            //  """,
+            // """
+            //     {
+            //         "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
+            //         "assetId": "1e049c87-ce56-4c54-afc8-0c5a01a97bf3",
+            //         "originalAssetBlobName": "original",
+            //         "sourceStorageAccount": "cheuw001assetsstcool",
+            //         "destinationStorageAccount": "cheuw001assetssthot",
+            //         "outFilePrefix": "boatnewyork",
+            //         "commandArgs": [
+            //             {
+            //             "outFileOptions": "-vf fps=1/4",
+            //             "outFileName": "boatnewyork_%04d.png"
+            //             },
+            //             {
+            //             "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 1280x720",
+            //             "outFileName": "boatnewyork_720p.mp4"
+            //             }
+            //         ]
+            //     }
+            // """,
+            // """
+            //     {
+            //         "assetContianerName": "originals-de1885b94150-d6f6b9f9-f2eb-42cf-96c5-fe0be098fef3",
+            //         "assetId": "66403f95-2ded-496c-bb50-38713bbe2000",
+            //         "originalAssetBlobName": "original",
+            //         "sourceStorageAccount": "cheuw001assetsstcool",
+            //         "destinationStorageAccount": "cheuw001assetssthot",
+            //         "outFilePrefix": "waterstream",
+            //         "commandArgs": [
+            //             {
+            //             "outFileOptions": "-vf fps=1/2",
+            //             "outFileName": "waterstream_%04d.png"
+            //             },
+            //             {
+            //             "outFileOptions": "-vcodec libx264 -crf 28 -preset ultrafast -c:a copy -s 720x1280",
+            //             "outFileName": "waterstream_720p.mp4"
+            //             }
+            //         ]
+            //     }
+            // """
         };
     }
 }
--- src/videoprocessor.eventhub/ScaledJobHostedService.cs
+++ src/videoprocessor.eventhub/ScaledJobHostedService.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,10 +17,16 @@ namespace videoprocessor.eventhub;
 internal sealed class ScaledJobHostedService : BackgroundService
 {
     private const string EventHubName = "videopreview";
+    private const string EventHubConsumer = "videoprevieweventhandler";
+    private const int TerminationWaitSeconds = 60;
+    private const int EventProcessCheckIntervalSeconds = 5;
 
     private readonly ILogger<ScaledJobHostedService> _logger;
     private readonly IHostApplicationLifetime _applicationLifetime;
     private readonly IConfiguration _configuration;
+
+    private bool _terminateIntiated;
+    private bool _continueProcessing;
 
     public ScaledJobHostedService(
         ILogger<ScaledJobHostedService> logger,
@@ -26,18 +37,86 @@ internal sealed class ScaledJobHostedService : BackgroundService
         _applicationLifetime = applicationLifetime;
         _configuration = configuration;
     }
-    
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
 
         string eventhubNamespace = _configuration["EventHubNamespaceName-1"];
         _logger.LogInformation($"Video processing job started for {eventhubNamespace}.");
 
-        _logger.LogInformation($"Video processing job running here.");
+        DefaultAzureCredential azureCredentials = new DefaultAzureCredential(
+                new DefaultAzureCredentialOptions
+                {
+                    TenantId = _configuration["AadTenantId"]
+                });
 
+        BlobContainerClient storageClient = new BlobContainerClient(
+        new Uri($"https://{_configuration["EventHubStorageName"]}.blob.core.windows.net/{EventHubConsumer}-{EventHubName}"),
+        azureCredentials);
+
+        var processor = new EventProcessorClient(
+            storageClient,
+            EventHubConsumerClient.DefaultConsumerGroupName,
+            $"{eventhubNamespace}.servicebus.windows.net",
+            $"{EventHubName}",
+            azureCredentials);
+
+
+        // Register handlers for processing events and handling errors
+        processor.ProcessEventAsync += ProcessEventHandler;
+        processor.ProcessErrorAsync += ProcessErrorHandler;
+
+        // Start the processing
+        await processor.StartProcessingAsync();
+        _continueProcessing = true;
+        _terminateIntiated = false;
+
+        while (_continueProcessing)
+        {
+            if (_terminateIntiated)
+            {
+                _continueProcessing = false;
+                _logger.LogInformation("Video processing job termination intiated...");
+                await Task.Delay(TimeSpan.FromSeconds(TerminationWaitSeconds));
+            }
+            else
+            {
+                // Check event processing status
+                await Task.Delay(TimeSpan.FromSeconds(EventProcessCheckIntervalSeconds));
+                _logger.LogInformation($"Video processing job running...");
+            }
+        }
+
+        // Stop the processing
+        await processor.StopProcessingAsync();
         _logger.LogInformation("Video processing job terminated.");
-        base.StopAsync(stoppingToken);
-        return Task.CompletedTask;
+        _applicationLifetime.StopApplication();
+    }
 
+    private Task ProcessEventHandler(ProcessEventArgs eventArgs)
+    {
+        if (_terminateIntiated)
+        {
+            _continueProcessing = true;
+            _terminateIntiated = false;
+            _logger.LogInformation("Video processing job termination cancelled...");
+        }
+
+        // Write the body of the event to the console window
+        Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+        Console.WriteLine("Processing...");
+        Task.Delay(TimeSpan.FromSeconds(10)); // This where we call video process service to generate previews
+
+        _terminateIntiated = true;
+        return Task.CompletedTask;
+    }
+
+    private Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+    {
+        // Write details about the error to the console window
+        Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+        Console.WriteLine(eventArgs.Exception.Message);
+        _terminateIntiated = true;
+        return Task.CompletedTask;
     }
 }
